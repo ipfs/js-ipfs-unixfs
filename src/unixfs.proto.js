3b3f851dd88b8606691d438e00a1ad166f8b6e4a
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-08-30 12:16:15 -0700

    feat: file metadata and permissions in protobuf
    This is based on the upcoming spec spec change https://github.com/ipfs/specs/pull/220

--- src/unixfs.proto.js
+++ src/unixfs.proto.js
@@ -17,6 +17,15 @@ module.exports = `message Data {
 
   optional uint64 hashType = 5;
   optional uint64 fanout = 6;
+
+  optional uint32 mtime = 7;
+	optional uint32 atime = 8;
+	optional uint32 ctime = 9;
+		
+	/* file permissions */
+	optional uint32 permissions = 10;
+	optional uint32 uid = 11;
+	optional uint32 gid = 12;
 }
 
 message Metadata {

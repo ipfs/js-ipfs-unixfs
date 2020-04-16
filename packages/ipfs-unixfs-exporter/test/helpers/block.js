e7989c79840b6d0746bf97df83d6c13e6198bee4
achingbrain <alex@achingbrain.net>
2020-04-16 09:59:57 +0100

    chore: fix up exporter tests

---
+++ packages/ipfs-unixfs-exporter/test/helpers/block.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const DAG_PB = require('ipld-dag-pb')
+const multicodec = require('multicodec')
+const mh = require('multihashing-async').multihash
+
+module.exports = (ipld) => {
+  // make ipld behave like the block api, some tests need to pull
+  // data from ipld so can't use use a simple hash
+  return {
+    put: async (buf, { cid }) => {
+      const multihash = mh.decode(cid.multihash)
+
+      if (cid.codec === 'dag-pb') {
+        buf = DAG_PB.util.deserialize(buf)
+      }
+
+      await ipld.put(buf, cid.codec === 'dag-pb' ? multicodec.DAG_PB : multicodec.RAW, {
+        cidVersion: cid.version,
+        hashAlg: multihash.code
+      })
+
+      return { cid, data: buf }
+    },
+    get: async (cid, options) => {
+      const node = await ipld.get(cid, options)
+
+      if (cid.codec === 'dag-pb') {
+        return node.serialize()
+      }
+
+      return { cid, data: node }
+    }
+  }
+}

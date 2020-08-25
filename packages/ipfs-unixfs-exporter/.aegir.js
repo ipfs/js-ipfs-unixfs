47fd32700af2ae6b6d15a65364a949970a62d440
achingbrain <alex@achingbrain.net>
2020-08-25 12:09:03 +0100

    chore: update deps

--- packages/ipfs-unixfs-exporter/.aegir.js
+++ packages/ipfs-unixfs-exporter/.aegir.js
@@ -3,5 +3,14 @@
 module.exports = {
   karma: {
     browserNoActivityTimeout: 1000 * 1000,
+  },
+  webpack: {
+    node: {
+      // needed by the cbor module
+      stream: true,
+
+      // needed by the core-util-is module
+      Buffer: true
+    }
   }
 }

7ded672c3d417ebabe705bda2a9218442378c529
Alan Shaw <alan.shaw@protocol.ai>
2023-02-28 13:42:59 +0000

    fix: parallelise loading of dag-pb links in directories when exporting

--- packages/ipfs-unixfs-exporter/src/resolvers/unixfs-v1/content/hamt-sharded-directory.ts
+++ packages/ipfs-unixfs-exporter/src/resolvers/unixfs-v1/content/hamt-sharded-directory.ts
@@ -1,3 +1,6 @@
+import parallel from 'it-parallel'
+import { pipe } from 'it-pipe'
+import map from 'it-map'
 import { decode, PBNode } from '@ipld/dag-pb'
 import type { Blockstore } from 'interface-blockstore'
 import type { ExporterOptions, Resolve, UnixfsV1DirectoryContent, UnixfsV1Resolver } from '../../../index.js'
@@ -22,22 +25,30 @@ const hamtShardedDirectoryContent: UnixfsV1Resolver = (cid, node, unixfs, path, 
 async function * listDirectory (node: PBNode, path: string, resolve: Resolve, depth: number, blockstore: Blockstore, options: ExporterOptions): UnixfsV1DirectoryContent {
   const links = node.Links
 
-  for (const link of links) {
-    const name = link.Name != null ? link.Name.substring(2) : null
+  const results = pipe(
+    links,
+    source => map(source, link => {
+      return async () => {
+        const name = link.Name != null ? link.Name.substring(2) : null
 
-    if (name != null && name !== '') {
-      const result = await resolve(link.Hash, name, `${path}/${name}`, [], depth + 1, blockstore, options)
+        if (name != null && name !== '') {
+          const result = await resolve(link.Hash, name, `${path}/${name}`, [], depth + 1, blockstore, options)
 
-      yield result.entry
-    } else {
-      // descend into subshard
-      const block = await blockstore.get(link.Hash)
-      node = decode(block)
+          return { entries: result.entry == null ? [] : [result.entry] }
+        } else {
+          // descend into subshard
+          const block = await blockstore.get(link.Hash)
+          node = decode(block)
 
-      for await (const file of listDirectory(node, path, resolve, depth, blockstore, options)) {
-        yield file
+          return { entries: listDirectory(node, path, resolve, depth, blockstore, options) }
+        }
       }
-    }
+    }),
+    source => parallel(source, { ordered: true })
+  )
+
+  for await (const { entries } of results) {
+    yield * entries
   }
 }
 

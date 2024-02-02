81e85c8a6798a8f76e11bdf2eebe9bfa2d9cb5a4
Alex Potsides <alex@achingbrain.net>
2024-02-02 16:23:49 +0100

    feat: add json resolver (#400)
    The json codec is included in the multiformats module so no extra
    deps are needed.
    
    Similar to #397

--- packages/ipfs-unixfs-exporter/src/resolvers/json.ts
+++ packages/ipfs-unixfs-exporter/src/resolvers/json.ts
@@ -1,10 +1,10 @@
-import * as dagCbor from '@ipld/dag-cbor'
+import * as json from 'multiformats/codecs/json'
 import { resolveObjectPath } from '../utils/resolve-object-path.js'
 import type { Resolver } from '../index.js'
 
 const resolve: Resolver = async (cid, name, path, toResolve, resolve, depth, blockstore, options) => {
   const block = await blockstore.get(cid, options)
-  const object = dagCbor.decode<any>(block)
+  const object = json.decode<any>(block)
 
   return resolveObjectPath(object, block, cid, name, path, toResolve, depth)
 }
